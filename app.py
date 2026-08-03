a9e39d1e61215560fe8e1e5a5fbf5a27e5ad384c
Leoz1n2008 <leonardo.n.justino@aluno.senai.br>
2026-08-03 12:03:31 -0300

    .

---
+++ app.py
@@ -0,0 +1,20 @@
+from SRC import create_app
+
+app = create_app()
+
+
+CONNECTION:
+from dotenv import load_dotenv
+import os 
+
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+db = SQLAlchemy()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('URL_DATABASE')
+
+    #Desabilita o rastreio de modificações dos objetos
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
